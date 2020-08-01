3500d02b95c9cbd7c5f1d3a0d252fe1319377ef6
Jorge Filipe <jorgefilipedasilva@gmail.com>
2020-08-01 15:08:58 -0300

    Start
    iniciando

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.alura.com.br/">
+        <img height="40" src={Logo} alt="Logo Alura" />
+      </a>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
