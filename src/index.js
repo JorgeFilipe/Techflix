a170c009317d91256706478a10ae36edb244c2c1
Jorge Filipe <jorgefilipedasilva@gmail.com>
2020-08-02 17:23:13 -0300

    Instalando o route-react-dom

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function CadastroVideo() {
+  return (
+    <div>
+      Página de Cadastro de Vídeo
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/" component={App} />
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
