953e01e5f700932af1e21c95b6589e6a04834012
Jorge Filipe <jorgefilipedasilva@gmail.com>
2020-08-07 15:12:09 -0300

    Criando o formulário de cadastro de categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,7 +7,21 @@ function CadastroCategoria() {
     return (
         <PageDefault>
             <h1>Cadastro de Categorias</h1>
-
+            <form>
+                <table border="0">
+                    <tr>
+                        <td>Nome da Categoria:</td>
+                        <td>
+                            <input type="text" />
+                        </td>
+                        <td>
+                            <button>
+                                Cadastrar
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+            </form>
             <Link to="/">
                 Ir para Home
             </Link>
