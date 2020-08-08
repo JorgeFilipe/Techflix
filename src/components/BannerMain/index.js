ebaa6724c5ceef892663c2d5014500c8978ddc9e
Jorge Filipe <jorgefilipedasilva@gmail.com>
2020-08-08 12:58:02 -0300

    Remoção dos Warnings dos Components

--- src/components/BannerMain/index.js
+++ src/components/BannerMain/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import VideoIframeResponsive from './components/VideoIframeResponsive';
 import { BannerMainContainer, ContentAreaContainer, WatchButton } from './styles';
 
