c9b5b0ab6271ade1dada2e9b14a8b2be9939bfc9
Jorge Filipe <jorgefilipedasilva@gmail.com>
2020-08-09 08:48:11 -0300

    refatorando o código fonte

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -4,34 +4,77 @@ import { Link } from 'react-router-dom';
 import './index.css';
 
 function CadastroCategoria() {
+    const [categorias, setCategorias] = useState(['Teste']);
     const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');
     return (
         <PageDefault>
             <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
-            <form>
-                {/* State */}
-                <table border="0">
-                    <tr>
-                        <td>Nome da Categoria:</td>
-                        <td>
-                            <input
-                                type="text"
-                                value={nomeDaCategoria}
-                                onChange={
-                                    function funcaoHandler(){
-                                        console.log('[nomeDaCategoria]', nomeDaCategoria);
-                                    }
+            <form onSubmit={function handleSubmit(infosDoEvento) {
+                infosDoEvento.preventDefault();
+                setCategorias([
+                    ...categorias,
+                    nomeDaCategoria
+                ]);
+            }}>
+                <div>
+                    <label>
+                        Nome da Categoria:
+                        <input
+                            type="text"
+                            value={nomeDaCategoria}
+                            onChange={
+                                function funcaoHandler(infosDoEvento) {
+                                    setNomeDaCategoria(infosDoEvento.target.value);
                                 }
-                            />
-                        </td>
-                        <td>
-                            <button>
-                                Cadastrar
+                            }
+                        />
+                    </label>
+                </div>
+
+                <div>
+                    <label>
+                        Descrição:
+                        <textarea
+                            type="text"
+                            value={nomeDaCategoria}
+                            onChange={
+                                function funcaoHandler(infosDoEvento) {
+                                    setNomeDaCategoria(infosDoEvento.target.value);
+                                }
+                            }
+                        />
+                    </label>
+                    
+                    <label>
+                        Cor:
+                        <input
+                            type="color"
+                            value={nomeDaCategoria}
+                            onChange={
+                                function funcaoHandler(infosDoEvento) {
+                                    setNomeDaCategoria(infosDoEvento.target.value);
+                                }
+                            }
+                        />
+                    </label>
+                </div>
+                <button>
+                    Cadastrar
                             </button>
-                        </td>
-                    </tr>
-                </table>
+
             </form>
+
+            <ul>
+                {categorias.map((categoria, indice) => {
+                    return (
+                        <li key={`${categoria}${indice}`}>
+                            {categoria}
+                        </li>
+                    )
+                })
+                }
+            </ul>
+
             <Link to="/">
                 Ir para Home
             </Link>
