53ca4c0ad52f902226172230752cc79cc662dca0
Jorge Filipe da Silva Macedo <jorgefilipedasilva@gmail.com>
2020-09-09 17:50:48 -0300

    Alts no FormField Component

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,11 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function FormField({ label, type, name, value, onChange }) {
+function FormField({ label,
+    type,
+    name,
+    value,
+    onChange }) {
+    const fieldId = `id_${name}`;
+    console.log(type);
     return (
         <div>
-            <label>
+            <label
+                htmlFor={fieldId}
+            >
                {label}: 
                 <input
+                    id={fieldId}
                     type={type}
                     value={value}
                     name={name}
@@ -14,6 +24,18 @@ function FormField({ label, type, name, value, onChange }) {
             </label>
         </div>
     )
+}
+
+FormField.defaulProps = {
+    type: 'text',
+}
+
+FormField.propTypes = {
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    //name: PropTypes.string.isRequired,
+    //value: PropTypes.string.isRequired,
+    //onChange: PropTypes.string.isRequired
 }
 
 export default FormField;
