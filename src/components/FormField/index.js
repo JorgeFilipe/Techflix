240799012cc3083a36df48542d725094c60e79b6
Jorge Filipe da Silva Macedo <jorgefilipedasilva@gmail.com>
2020-10-03 18:04:34 -0300

    Alterando a Sintax no FormField.js

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function FormField({ label, type, name, value, onChange }) {
+function FormField({ label,
+    type,
+    name,
+    value,
+    onChange }) {
+
+    const fieldId = `id_${name}`;
+
     return (
         <div>
-            <label>
-               {label}: 
+            <label htmlFor={fieldId} >
+                {label}:
                 <input
                     type={type}
                     value={value}
@@ -14,6 +22,20 @@ function FormField({ label, type, name, value, onChange }) {
             </label>
         </div>
     )
+}
+
+FormField.defaultProps = {
+    type: 'text',
+    value: '',
+    onChange: () => {},
+}
+
+FormField.protoTypes = {
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    onChange: PropTypes.string.func,
 }
 
 export default FormField;
